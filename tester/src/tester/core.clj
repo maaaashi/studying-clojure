a7e5d30d3817b815ac8e8f4b718b97a8db36e2ff
maaaashi <doyaseizin.7@gmail.com>
2024-01-20 23:54:59 +0900

    add plus and minus

--- tester/src/tester/core.clj
+++ tester/src/tester/core.clj
@@ -4,3 +4,13 @@
   "This is a plus function example."
   [& args]
   (reduce + args))
+
+(defn plus_arbitrary
+  "This is a plus function example."
+  [& args]
+  (reduce +' args))
+
+(defn minus
+  "This is a minus function example."
+  [a & args]
+  (- a (reduce + args)))

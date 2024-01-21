c85aead80aa83ca298591ca9ad2e06180469be13
maaaashi <doyaseizin.7@gmail.com>
2024-01-21 18:00:22 +0900

    add few functions

--- tester/src/tester/core.clj
+++ tester/src/tester/core.clj
@@ -5,7 +5,7 @@
   [& args]
   (reduce + args))
 
-(defn plus_arbitrary
+(defn plus_'
   "This is a plus function example."
   [& args]
   (reduce +' args))
@@ -14,3 +14,13 @@
   "This is a minus function example."
   [a & args]
   (- a (reduce + args)))
+
+(defn minus_'
+  "This is a minus function example."
+  [a & args]
+  (-' a (reduce +' args)))
+
+(defn sread_first
+  "This is a sread_first function example."
+  [x]
+  (-> x (plus 5) (minus 2)))

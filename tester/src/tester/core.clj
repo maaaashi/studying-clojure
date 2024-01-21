ced5763e7be14de71753ffada9f87f11ee55eae4
maaaashi <doyaseizin.7@gmail.com>
2024-01-22 00:10:40 +0900

    add few functions

--- tester/src/tester/core.clj
+++ tester/src/tester/core.clj
@@ -24,3 +24,20 @@
   "This is a sread_first function example."
   [x]
   (-> x (plus 5) (minus 2)))
+
+(defn sread_last
+  "This is a sread_last function example."
+  [x]
+  (->> x (plus 5) (minus 100)))
+
+(defn dot_java_math_random
+  "This is a dot function example." 
+  []
+  (. Math random))
+
+(defn double_dot_java_String_UpperCase
+  "This is a double_dot function example"
+  [x]
+  (.. (StringBuilder. x)
+      (toString)
+      (toUpperCase)))
